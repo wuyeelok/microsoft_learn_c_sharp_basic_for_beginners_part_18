45d0a2b4577a26cdf918e0cbc852e658a2ec8e1b
wuyeelok <wuyeelok@gmail.com>
2025-02-22 10:23:53 +0800

    1st C# constructor

--- ConsoleApp16/Program.cs
+++ ConsoleApp16/Program.cs
@@ -1,18 +1,22 @@
 ﻿Console.WriteLine("Hello World! OOP");
 
-var p = new Person()
-{
-    firstName = "John",
-    lastName = "Doe",
-    birthDate = new DateOnly(1980, 1, 1)
-};
+var p = new Person("John", "Doe", new DateOnly(1990, 1, 1));
 
-Console.WriteLine($"{p.firstName} {p.lastName} was born on {p.birthDate}");
+
+// Console.WriteLine($"{p.firstName} {p.lastName} was born on {p.birthDate}");
 
 public class Person
 {
-    public string firstName;
-    public string lastName;
-    public DateOnly birthDate;
+    public Person(string firstName, string lastName, DateOnly birthDate)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+    }
+
+
+    private string firstName;
+    private string lastName;
+    private DateOnly birthDate;
 }
 
